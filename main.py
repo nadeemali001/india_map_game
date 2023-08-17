850d816ba9a12ef449e22e11ab9cbb4428760319
Nadeem Ali <nadeemali001@gmail.com>
2023-08-17 23:41:35 +0530

    final commit

--- main.py
+++ main.py
@@ -6,25 +6,33 @@ scn = Screen()
 
 img = 'india_map.gif'
 ifile = '28_states.csv'
+ofile = 'Ramining_states.csv'
 
 scn.title("India States Quiz")
 scn.addshape(img)
 map.shape(img)
-
+guessed_states = []
 game_is_on = True
 c = 5
 
 df = pd.read_csv(ifile)
-all_s = df.states.str.lower()
+all_s = df.states
 all_states = all_s.to_list()
 
 while game_is_on:
-    ans_state = scn.textinput(title="Guess the State", prompt="What's the next state name?").lower()
+    ans_state = scn.textinput(title="Guess the State", prompt="What's the next state name?").title()
+    if ans_state == 'Exit':
+        for s in all_s:
+            if s not in ans_state:
+                with open(ofile, "a+") as rem:
+                    rem.write(s+"\n")
+        break
     if ans_state in all_states:
+        guessed_states.append(ans_state)
         t = Turtle()
         t.hideturtle()
         t.penup()
-        data = df[df.states.str.lower() == ans_state.lower()]
+        data = df[df.states == ans_state]
         t.goto(int(data.x), int(data.y))
         t.write(data.states.item())
     else:
@@ -36,26 +44,3 @@ while game_is_on:
         t.write(f"Retry Count left {c}")
         if c == 0:
            game_is_on = False
-
-
-# while game_is_on:
-#     ans_state = scn.textinput(title="Guess the State", prompt="What's the next state name?")
-#     t = Turtle()
-#     t.hideturtle()
-#     t.penup()
-#     df = pd.read_csv(ifile)
-#     res = (df.loc[df['states'] == ans_state])
-#     if not res.empty:
-#         final = res.values.tolist()
-#         x=final[0][1]
-#         y=final[0][2]
-#         t.goto(x, y)
-#         t.write(final[0][0])
-#     else:
-#         c = c - 1
-#         t.goto(150, 150)
-#         t.write(f"Retry Count left {c}")
-#         if c == 0:
-#             game_is_on = False
-
-scn.exitonclick()
